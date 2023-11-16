5c401e80307d8eb1ed9c8fa5e6a8e265ed748fb8
ggoreb <seorab@naver.com>
2023-11-16 12:35:47 +0900

    데이터 목록 보여주기

--- src/main/java/com/example/basic/controller/UploadController.java
+++ src/main/java/com/example/basic/controller/UploadController.java
@@ -13,6 +13,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -28,6 +29,14 @@ import com.fasterxml.jackson.core.util.ByteArrayBuilder;
 
 @Controller
 public class UploadController {
+
+  @GetMapping("/article")
+  public String article(Model model) {
+    List<Article> list = 
+      articleRepository.findAll();
+    model.addAttribute("list", list);
+    return "article/list";
+  }
 
   @GetMapping("/article/add")
   public String articleAdd() {
